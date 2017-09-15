fe4114a3d0ddec70b707c46bcd5a922fdf6c7e68
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-15 17:47:08 +0100

    i can do this better but fuck it

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import urllib
 from time import sleep
 
 timeout = 0;
@@ -15,5 +16,26 @@ def timeout(timeout, times):
 
         delay = 0.5
 
-        print "\nNow tinder is really god damn easy"
-        
+        print ("\nNow tinder is really god damn easy")
+        print("Swiping all users (have fun!)")
+
+        counter = 0
+    while True:
+        debug
+        try:
+            r_recs = urllib.Request('https://api.gotinder.com/user/recs')
+            resp_recs = json.load(urllib.urlopen(r_recs).read())
+            debug = resp_recs
+            if 'message' in resp_recs:
+                if resp_recs['message'] == 'rescs exhausted':
+                    print ('Nothing new around ;(')
+                    timeout(times, timeout)
+                    timeout += 1
+                    continue
+
+                    hoes = resp_recs['results']
+                    for hoe in hoes:
+                        sleep(delay)
+                        id = hoe['_id']
+                        r_like = urllib.Request('https://api.gotinder.com/like/' + id)
+                        status = json.load(urllib.urlopen(r_like).read())['match']
