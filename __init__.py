9809f12921d1c96a21c1d560d1451696e59b842f
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-15 19:22:35 +0100

    bug fixes + reformat of some keywords

--- __init__.py
+++ __init__.py
@@ -4,19 +4,21 @@ import urllib
 from time import sleep
 
 timeout = 0;
-times = [60, 300, 600] #times are: 1) 1 minute 2) 5 minutes 3) 10 minutes
+times = [60, 300, 600]  # times are: 1) 1 minute 2) 5 minutes 3) 10 minutes
 
-def timeout(timeout, times):
-    if timeout > len(times):
+
+def timeout(_timeout, _times):
+    if _timeout > len(_times):
         print('Timed out. Exiting')
         sys.exit(1)
     else:
-        print("Timed out. Waiting...", str(timeout(times)) / 60), " before resuming"
-        sleep(times[timeout])
+        assert isinstance(_times, object)
+        print("Timed out. Waiting...", str(_timeout(_times)) / 60), " before resuming"
+        sleep(_times[_timeout])
 
         delay = 0.5
 
-        print ("\nNow tinder is really god damn easy")
+        print("\nNow tinder is really god damn easy")
         print("Swiping all users (have fun!)")
 
         counter = 0
@@ -28,9 +30,9 @@ def timeout(timeout, times):
             debug = resp_recs
             if 'message' in resp_recs:
                 if resp_recs['message'] == 'rescs exhausted':
-                    print ('Nothing new around ;(')
-                    timeout(times, timeout)
-                    timeout += 1
+                    print('Nothing new around ;(')
+                    _timeout(_times, _timeout)
+                    _timeout += 1
                     continue
 
                     hoes = resp_recs['results']
@@ -41,7 +43,7 @@ def timeout(timeout, times):
                         status = json.load(urllib.urlopen(r_like).read())['match']
                         if status:
                             print("Matched with ", hoe['name'])
-                            timeout = 0
+                            _timeout = 0
                             counter += 1
                             sys.stdout.write("Swiped users: " + str(counter))
                             sys.stdout.flush()
